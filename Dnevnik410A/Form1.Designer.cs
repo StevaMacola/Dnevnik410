82632a38e225d13980baa0f3a3855855e4ef0d0d
ucenik <ucenik@DESKTOP-ASD8HJ9>
2025-10-29 11:06:26 +0100

    Add project files.

---
+++ Dnevnik410A/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Dnevnik410A
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IDtB = new System.Windows.Forms.TextBox();
+            this.prezimetB = new System.Windows.Forms.MaskedTextBox();
+            this.jmbgtB = new System.Windows.Forms.TextBox();
+            this.mailtB = new System.Windows.Forms.TextBox();
+            this.lozinkatB = new System.Windows.Forms.TextBox();
+            this.imetB = new System.Windows.Forms.TextBox();
+            this.adresatB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // IDtB
+            // 
+            this.IDtB.Location = new System.Drawing.Point(595, 78);
+            this.IDtB.Name = "IDtB";
+            this.IDtB.Size = new System.Drawing.Size(84, 20);
+            this.IDtB.TabIndex = 0;
+            // 
+            // prezimetB
+            // 
+            this.prezimetB.Location = new System.Drawing.Point(595, 183);
+            this.prezimetB.Name = "prezimetB";
+            this.prezimetB.Size = new System.Drawing.Size(84, 20);
+            this.prezimetB.TabIndex = 1;
+            // 
+            // jmbgtB
+            // 
+            this.jmbgtB.Location = new System.Drawing.Point(595, 298);
+            this.jmbgtB.Name = "jmbgtB";
+            this.jmbgtB.Size = new System.Drawing.Size(84, 20);
+            this.jmbgtB.TabIndex = 2;
+            // 
+            // mailtB
+            // 
+            this.mailtB.Location = new System.Drawing.Point(595, 354);
+            this.mailtB.Name = "mailtB";
+            this.mailtB.Size = new System.Drawing.Size(84, 20);
+            this.mailtB.TabIndex = 3;
+            // 
+            // lozinkatB
+            // 
+            this.lozinkatB.Location = new System.Drawing.Point(595, 411);
+            this.lozinkatB.Name = "lozinkatB";
+            this.lozinkatB.Size = new System.Drawing.Size(84, 20);
+            this.lozinkatB.TabIndex = 4;
+            // 
+            // imetB
+            // 
+            this.imetB.Location = new System.Drawing.Point(595, 130);
+            this.imetB.Name = "imetB";
+            this.imetB.Size = new System.Drawing.Size(84, 20);
+            this.imetB.TabIndex = 5;
+            // 
+            // adresatB
+            // 
+            this.adresatB.Location = new System.Drawing.Point(595, 234);
+            this.adresatB.Name = "adresatB";
+            this.adresatB.Size = new System.Drawing.Size(84, 20);
+            this.adresatB.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(501, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(501, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "ime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(501, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "prezime";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(501, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "adresa";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(501, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "jmbg";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(501, 354);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "e-mail";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(501, 411);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "lozinka";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(644, 414);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 566);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "<<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(245, 566);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(428, 566);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "dodaj";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(604, 566);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "menjaj";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(787, 566);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 26;
+            this.button5.Text = "brisi";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(943, 566);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 27;
+            this.button6.Text = ">";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1107, 566);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 28;
+            this.button7.Text = ">>";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1397, 626);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.adresatB);
+            this.Controls.Add(this.imetB);
+            this.Controls.Add(this.lozinkatB);
+            this.Controls.Add(this.mailtB);
+            this.Controls.Add(this.jmbgtB);
+            this.Controls.Add(this.prezimetB);
+            this.Controls.Add(this.IDtB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox IDtB;
+        private System.Windows.Forms.MaskedTextBox prezimetB;
+        private System.Windows.Forms.TextBox jmbgtB;
+        private System.Windows.Forms.TextBox mailtB;
+        private System.Windows.Forms.TextBox lozinkatB;
+        private System.Windows.Forms.TextBox imetB;
+        private System.Windows.Forms.TextBox adresatB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Dnevnik410A/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Dnevnik410A
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string CS = "Data Source=DESKTOP-ASD8HJ9\\SQLEXPRESS;Initial catalog=dnevnik410A;Integrated security=true";
+            SqlConnection veza = new SqlConnection(CS);
+            SqlDataAdapter DA = new SqlDataAdapter("Select * from osoba", veza);
+            DataTable tabela = new DataTable();
+            DA.Fill(tabela);
+            IDtB.Text = tabela.Rows[0][0].ToString();
+            imetB.Text = tabela.Rows[0][1].ToString();
+            prezimetB.Text = tabela.Rows[0][2].ToString();
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
